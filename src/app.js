958aec9fe10c8435a2df3c23ea41adb9c922c8d4
TamimiDuaa <tamimiduaa18@gmail.com>
2024-08-12 01:25:02 -0400

    add the change color theme

--- src/app.js
+++ src/app.js
@@ -24,46 +24,4 @@ function changeProfile(url){
 
 
 
-try {
-  
-const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: '# of applications',
-      data: [2, 19, 4, 3, 2, 10],
-      backgroundColor: [ // Custom background colors for the bars
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [ // Custom border colors for the bars
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-      borderWidth: 0.2
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
-} catch (error) {
-console.log(error);
-}
 
